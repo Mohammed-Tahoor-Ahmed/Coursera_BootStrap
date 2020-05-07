51991e51c1f63d64808f8c2e744bedc7be771dd2
Mohammed-Tahoor-Ahmed <mdtahoor1112@gmail.com>
2020-05-08 00:50:30 +0530

    week04

---
+++ Bootstrap4/conFusion/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(".carousel").carousel({interval:1000});
+    $("#carousel-button").click(function() {
+        if($('#carousel-button').children('span').hasClass('fa fa-pause')) {
+            $(".carousel").carousel('pause');
+            $('#carousel-button').children('span').removeClass('fa fa-pause');
+            $('#carousel-button').children('span').addClass('fa fa-play');
+        }
+        else if($('#carousel-button').children('span').hasClass('fa fa-play')) {
+            $(".carousel").carousel('cycle');
+            $('#carousel-button').children('span').removeClass('fa fa-play');
+            $('#carousel-button').children('span').addClass('fa fa-pause');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#login-button').click(function() {
+        $('#loginModal').modal();
+    });
+    $('#reserve-button').click(function() {
+        $('#reserveModal').modal('toggle');
+    })
+});
